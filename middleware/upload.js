ca1bcabbd47de797ee8a40d6d094daa155b6f041
Somniloquios <europa.cid@gmail.com>
2026-05-09 17:57:30 -0300

    fix cloudinary upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,13 +3,29 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
+
   cloudinary,
-  params: {
-    folder: "darkstories",
-    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+
+  params: async (req, file) => {
+
+    return {
+
+      folder: "darkstories",
+
+      allowed_formats: [
+        "jpg",
+        "jpeg",
+        "png",
+        "webp",
+      ],
+
+    };
   },
+
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+});
 
 export default upload;
